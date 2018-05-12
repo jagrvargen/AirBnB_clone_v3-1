9b4b9e493c27c798a6ba1f440720906f735db3de
Lee Gaines <eightlimbed@gmail.com>
2018-05-12 23:06:21 +0000

    API status circle working, Amenities checkboxes working

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,8 +1,37 @@
-// This script listens on each Amenity <input> field.
+// Listens on each Amenity <input> field.
 // If the checkbox is checked, the amenity is added to an object called 'selected'
 // If the amenity is unchecked, it is removed from 'selected'
 // The <H4> Amenity tag is updated dynamically when boxes are checked/unchecked
+$(document).ready(function () {
+  let amenity_ids = [];
+  let amenity_names = [];
+  $('input').on('click', function (event) {
+    if ($(this).prop('checked')) {
+      amenity_names.push($(this).attr('data-name'));
+      amenity_ids.push($(this).attr('data-id'));
+      let txt = amenity_names.join(', ');
+      if (txt.length > 25) {
+	$('.amenities h4').text(txt.substr(0,25) + '...');
+      } else {
+	$('.amenities h4').text(txt);
+      }
+      } else {
+      let name_index = amenity_names.indexOf($(this).attr('data-name'));
+      let id_index = amenity_ids.indexOf($(this).attr('data-id'));
+      amenity_names.splice(name_index, 1);
+      amenity_ids.splice(id_index, 1);
+      let txt = amenity_names.join(', ');
+      if (txt.length > 25) {
+        $('.amenities h4').text(txt.substr(0,25) + '....');
+      } else {
+        $('.amenities h4').text(txt);
+      }
+      }
+  })
+});
 
+// Toggles the API status circle in upper-right hand part of webpage
+// Red is API status is OK, grey if now.
 $(document).ready(function () {
     let url = 'http://0.0.0.0:5001/api/v1/status/';
     $.ajax({
