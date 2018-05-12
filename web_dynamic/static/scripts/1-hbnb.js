d7b685d75867a39993d98bd23743d5ec7fef26bf
jagrvargen <jagarthewolf@gmail.com>
2018-05-12 20:44:46 +0000

    fixed overflow problem with amenities div

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -5,14 +5,23 @@ $(document).ready(function () {
     if ($(this).prop('checked')) {
       amenity_names.push($(this).attr('data-name'));
       amenity_ids.push($(this).attr('data-id'));
-      $('.amenities h4').html(amenity_names.join(', '));
-      console.log('check1');
-    } else {
+      let txt = amenity_names.join(', ');
+      if (txt.length > 45) {
+	$('.amenities h4').text(txt.substr(0,45) + '....');
+      } else {
+	$('.amenities h4').text(txt);
+      }
+      } else {
       let name_index = amenity_names.indexOf($(this).attr('data-name'));
       let id_index = amenity_ids.indexOf($(this).attr('data-id'));
       amenity_names.splice(name_index, 1);
       amenity_ids.splice(id_index, 1);
-      $('.amenities h4').html(amenity_names.join(', '));
-    }
+      let txt = amenity_names.join(', ');
+      if (txt.length > 45) {
+        $('.amenities h4').text(txt.substr(0,45) + '....');
+      } else {
+        $('.amenities h4').text(txt);
+      }
+      }
   })
 });
